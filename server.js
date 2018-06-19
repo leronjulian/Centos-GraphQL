98197dca7763ca8c272f895153bcf8569f680c46
Leron K. Julian <leronjulian@hotmail.com>
2018-06-19 11:28:38 -0400

    Added Apollo Client

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var { buildSchema } = require('graphql');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
 var app = express();
 
 var books2;
 
-app.use('/graphql', (req, res, next) => {
+app.use('/graphiql', (req, res, next) => {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("books_db");
@@ -23,6 +27,7 @@ app.use('/graphql', (req, res, next) => {
 
 
 // Construct a schema, using GraphQL schema language
+/*
 var schema = buildSchema(`
   type Books {
     title: String!
@@ -32,22 +37,47 @@ var schema = buildSchema(`
   type Query{
     allBooks : [Books]
   }
-
 `);
+*/
+
+const typeDefs = `
+  type Books { title: String, author: String }
+  type Query { allBooks: [Books] }
+`;
 
 // The root provides a resolver function for each API endpoint
+/*
 var root = {
   allBooks: () => {
     return books2;
   },
 };
+*/
 
+const resolvers = {
+  Query: { allBooks: () => {return books2 } },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+/*
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 
 }));
+
+*/
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.listen(4000);
 
